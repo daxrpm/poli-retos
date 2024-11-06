31b4fa731cd0dd065d59175e39bf37daea463477
Dax <daxnavarrete@gmail.com>
2024-11-06 11:17:40 -0500

    fix app design

---
+++ poli_retos/src/bienvenida/Bienvenida.java
@@ -0,0 +1,14 @@
+package bienvenida;
+
+public class Bienvenida {
+
+    public static void imprimirG4conCaracteres() {
+        System.out.println("Grupo 4");
+        System.out.println("Integrantes:");
+        System.out.println("1. Navarrete Freire Ariel Dax");
+        System.out.println("2. PIllajo Pillajo Carolay Yajaira");
+        System.out.println("3. Pizarro Quezada Walter Josue");
+        System.out.println("4. Quinatoa Zapata Frank Jhofred");
+    }
+
+}
