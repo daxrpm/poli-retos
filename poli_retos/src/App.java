978582c4b053314fbc6a61f5a0968eba4b938f18
Matthrve <169124743+Matthrve@users.noreply.github.com>
2024-11-06 00:42:41 -0500

    Update App.java

--- poli_retos/src/App.java
+++ poli_retos/src/App.java
@@ -2,13 +2,16 @@ import arrays.*;
 import loading.*;
 import figuras.*;
 import series.*;
+import serieCaracteres.*;
+import recursion.*;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese su nivel/tamaño: ");
+        System.out.println("");
+        System.out.println("Ingrese su nivel/tamaño/número de serie: ");
         int nivel = sc.nextInt();
         SerieNumerica serieNumerica = new SerieNumerica(nivel);
         serieNumerica.g4_imprimeTodasSeries();
@@ -18,6 +21,15 @@ public class App {
         loading.g4_imprimirTodosLoading();
         Figuras figuras = new Figuras(nivel);
         figuras.g4_imprimirTodasFiguras();
-
+        SerieCaracteres serieCaracteres= new SerieCaracteres(n);
+        serieCaracteres.g4_imprimeTodasSeriesdeCaracteres();
+        System.out.println("Ingrese el primer valor para las recursiones; se tomará este valor para factorial(a) y conteo progresivo:");
+        int valor1 = scanner.nextInt();
+        System.out.println("Ingrese el segundo valor para las recursiones; se tomará este valor para conteo regresivo:");
+        int valor2 = scanner.nextInt();
+        Recursion recursion = new Recursion(valor1, valor2);
+        recursion.g4_imprimeTodasRecursiones();
+        System.out.println();
+scanner.close();
     }
 }
